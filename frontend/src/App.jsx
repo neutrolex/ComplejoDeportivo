80a852217febe4321a2ec339f700d44ea1c6b679
Neutrolex <60861853campos@gmail.com>
2026-08-22 09:34:32 -0500

    Agrega react-router-dom y separa /horarios (publico) de / (panel con login)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AuthProvider, useAuth } from './context/AuthContext'
 import Login from './components/Login'
 import PanelDisponibilidad from './components/PanelDisponibilidad'
+import HorariosPublicos from './components/HorariosPublicos'
 
-function Contenido() {
+function PanelConLogin() {
   const { autenticado, cerrarSesion } = useAuth()
 
   if (!autenticado) {
@@ -20,7 +22,13 @@ function Contenido() {
 function App() {
   return (
     <AuthProvider>
-      <Contenido />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/horarios" element={<HorariosPublicos />} />
+          <Route path="/" element={<PanelConLogin />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   )
 }
